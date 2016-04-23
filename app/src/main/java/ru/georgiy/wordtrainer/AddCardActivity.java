5a1bb97867c06c696c6e91ef4766834c9db170db
George <geobarit@gmail.com>
2016-04-23 16:14:13 +0300

    реализована возможность добавить слово и перевод в базу

--- app/src/main/java/ru/georgiy/wordtrainer/AddCardActivity.java
+++ app/src/main/java/ru/georgiy/wordtrainer/AddCardActivity.java
@@ -1,21 +1,27 @@
 package ru.georgiy.wordtrainer;
 
 import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
-import java.util.Scanner;
+import android.widget.Toast;
 
 /**
  * Created by Georgiy on 17.04.2016.
  */
-public class AddCardActivity extends Activity{
+public class AddCardActivity extends Activity implements View.OnClickListener{
 
     EditText engtext;
     EditText rustext;
     Button addBtn;
+    private SQLiteDatabase db;
+    private Cursor cursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -24,13 +30,54 @@ public class AddCardActivity extends Activity{
 
         engtext = (EditText)findViewById(R.id.editText);
         rustext = (EditText)findViewById(R.id.editText2);
-        addBtn = (Button)findViewById(R.id.button);
+        addBtn = (Button)findViewById(R.id.addBtn);
 
-        Scanner scanner = new Scanner(System.in);
-        String addword = scanner.nextLine();
-        engtext.getText();
+
+
+        //cursor = db.query("WORDS", new String[]{"ENG", "RUS"}, null, null, null, null, null);
+
+
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
     }
 
     public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.addBtn:
+                try {
+                    SQLiteOpenHelper cardDatabaseHelper=new CardDatabaseHelper(this);
+                    db = cardDatabaseHelper.getWritableDatabase();
+                    String eng = engtext.getText().toString();
+                    String rus = rustext.getText().toString();
+
+                    ContentValues new_word = new ContentValues();
+                    new_word.put("ENG", eng);
+                    new_word.put("RUS", rus);
+                    db.insert("WORDS", null, new_word);
+                    db.update("WORDS", new_word, "ENG =? AND RUS=?",new String[]{eng, rus});
+                    db.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Toast toast = Toast.makeText(this, "add word exception", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+
+//                Intent intent = new Intent(this,CardActivity.class);
+//                startActivity(intent);
+
+
+                finish();
+                break;
+
+        }
+    }
+    @Override
+    public  void onDestroy(){
+        super.onDestroy();
+        cursor.close();
+        db.close();
     }
 }
