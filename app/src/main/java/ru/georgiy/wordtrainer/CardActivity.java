d525b367333a9bfe03c49d789ffcc9f9784a5213
George <geobarit@gmail.com>
2016-04-23 16:16:52 +0300

    Добавлена обработка кнопок, сравнение введенных данных на русском со значением записи базы на английском

--- app/src/main/java/ru/georgiy/wordtrainer/CardActivity.java
+++ app/src/main/java/ru/georgiy/wordtrainer/CardActivity.java
@@ -1,10 +1,14 @@
 package ru.georgiy.wordtrainer;
 
 import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.provider.UserDictionary;
 import android.support.v7.widget.CardView;
 import android.view.KeyEvent;
 import android.view.View;
@@ -21,7 +25,7 @@ import java.util.Scanner;
 /**
  * Created by Georgiy on 16.04.2016.
  */
-public class CardActivity extends Activity {
+public class CardActivity extends Activity implements View.OnClickListener{
 
     Button prevBtn;
     Button checkBtn;
@@ -35,92 +39,125 @@ public class CardActivity extends Activity {
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.card_activity);
 
-        prevBtn = (Button)findViewById(R.id.prevBtn);
-        checkBtn = (Button)findViewById(R.id.checkBtn);
-        nextBtn = (Button)findViewById(R.id.nextBtn);
-        textView = (TextView)findViewById(R.id.txtView);
-        answer = (EditText) findViewById(R.id.inputSolution);  //ввод ответа
-        cardview = (CardView) findViewById(R.id.cv);
-
-        test = (TextView)findViewById(R.id.test);
-
+        prevBtn=(Button)findViewById(R.id.prevBtn);
+        checkBtn=(Button)findViewById(R.id.checkBtn);
+        nextBtn=(Button)findViewById(R.id.nextBtn);
+        textView=(TextView)findViewById(R.id.txtView);
+        answer=(EditText)findViewById(R.id.inputSolution);  //ввод ответа
+        cardview=(CardView)findViewById(R.id.cv);
+        test=(TextView)findViewById(R.id.test);
 
 
-        try {
-            SQLiteOpenHelper cardDatabaseHelper = new CardDatabaseHelper(this);
+        try{
+            SQLiteOpenHelper cardDatabaseHelper=new CardDatabaseHelper(this);
             db = cardDatabaseHelper.getReadableDatabase();
-            cursor = db.query("WORDS", new String[]{"ENG", "RUS"}, null, null, null, null, null);
-            if(cursor.moveToFirst()){
-                String engtext = cursor.getString(0);
-                String rustext = cursor.getString(1);
-                textView.setText(engtext);
-                //test.setText(rustext);
-            }
-        } catch (Exception e) {
+            cursor=db.query("WORDS",new String[]{"ENG","RUS"},null,null,null,null,null);
+                if(cursor.moveToFirst()){
+                    String engtext=cursor.getString(0);
+                    //String rustext=cursor.getString(1);
+                    textView.setText(engtext);
+                    //test.setText(rustext);
+                }
+        }catch(Exception e){
             e.printStackTrace();
-            Toast toast = Toast.makeText(this, "db exception", Toast.LENGTH_SHORT);
+            Toast toast=Toast.makeText(this,"db exception",Toast.LENGTH_SHORT);
             toast.show();
         }
 
-        checkBtn.setOnClickListener(new View.OnClickListener() {
+    }
 
-            @Override
-            public void onClick(View v) {
-                check();
-//                try {
-//                    Scanner scanner = new Scanner();
-//                    String s = scanner.nextLine();
-//                    //test.setText(s);
-//                }catch (Exception e){
-//                    test.setText("exception");
-//                }
-                // textView.setText("нажата кнопка проверить");
-            }
-        });
 
-//        answer.setOnKeyListener(new View.OnKeyListener() {
-//            @Override
-//            public boolean onKey(View v, int keyCode, KeyEvent event) {
-//                String input = answer.getText().toString();
-//                test.setText(input);
-//                return false;
-//            }
-//        });
+    private void previous() {
+        if(!cursor.isFirst()){
+            cursor.moveToPrevious();
+            String engtext=cursor.getString(0);
+            textView.setText(engtext);
+            cardview.setCardBackgroundColor(Color.rgb(0,150,200));
+        }
+    }
+
+    private void next() {
+        if(!cursor.isLast()){
+            cursor.moveToNext();
+            String engtext=cursor.getString(0);
+            textView.setText(engtext);
+            cardview.setCardBackgroundColor(Color.rgb(0,150,200));
+        }
     }
 
     public void check(){
-
         String rustext = cursor.getString(1);
-//        InputStreamReader in = new InputStreamReader(System.in);
-//        BufferedReader buf = new BufferedReader(in);
-       // try {
-//        Scanner scanner = new Scanner(System.in);
-//        String s = scanner.nextLine();
-//        test.setText(s);
-        //test.setText(rustext);
-
-        //String s = buf.readLine();
-           // test.setText(s);
-
         String s = answer.getText().toString();
         //test.setText(s);
             if (s.equals(rustext)) {
                     textView.setText("Верно");
+                    cardview.setCardBackgroundColor(Color.GREEN);
+                    cardview.animate();
                 } else {
                     textView.setText("Не верно");
+                    cardview.setCardBackgroundColor(Color.RED);
                 }
-
         }
-
 
     @Override
     public  void onDestroy(){
         super.onDestroy();
         cursor.close();
         db.close();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()) {
+            case (R.id.checkBtn):
+                check();
+                //textView.setText("check pressed");
+                break;
+            case (R.id.nextBtn):
+                next();
+                answer.setText("");
+                //textView.setText("next pressed");
+                break;
+            case (R.id.prevBtn):
+                previous();
+                answer.setText("");
+                //textView.setText("prev pressed");
+                break;
+            case R.id.fab:
+                Intent intent = new Intent(this, AddCardActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.fab_delete:
+                deleteWord();
+                break;
+            case R.id.fab_list:
+                Intent intent_list = new Intent(this,DataBaseList.class);
+                startActivity(intent_list);
+                break;
+            default:
+                Toast toast = Toast.makeText(this, "not press any button", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+    }
+
+    private void deleteWord() {
+        try {
+            String current = cursor.getString(cursor.getColumnIndex("ENG"));
+            SQLiteOpenHelper cardDatabaseHelper = new CardDatabaseHelper(this);
+            db = cardDatabaseHelper.getWritableDatabase();
+            db.delete("WORDS", "ENG = ?", new String[]{current});
+//            ContentValues removed = new ContentValues();
+//            removed.remove("ENG");
+//            db.update("WORDS", removed, null, null);
+            db.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast toast = Toast.makeText(this, "exception db", Toast.LENGTH_SHORT);
+            toast.show();
+        }
     }
 }
